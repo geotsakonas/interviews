fc84c7d9a2aece9357bc55756998edc11a17742a
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-05-18 13:18:30 +0300

    Update SparseMatrixMultiplication.java

--- company/linkedin/SparseMatrixMultiplication.java
+++ company/linkedin/SparseMatrixMultiplication.java
@@ -27,16 +27,7 @@ public class SparseMatrixMultiplication {
         int nB = B[0].length;
         int[][] C = new int[m][nB];
         
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(A[i][j] != 0) {
-                    for(int k = 0; k < nB; k++) {
-                        if(B[j][k] != 0) {
-                            C[i][k] += A[i][j] * B[j][k];
-                        }
-                    }
-                }
-            }
+        for(int i = 0
         }
         
         return C;
